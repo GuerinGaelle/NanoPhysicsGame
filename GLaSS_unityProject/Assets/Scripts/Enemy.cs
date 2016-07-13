6ccd67d86b387cce911553e6337b7286ec8eb9ec
Parvati or Vicky <Parvati or Vicky>
2016-07-13 15:15:46 +0200

    small changes

--- GLaSS_unityProject/Assets/Scripts/Enemy.cs
+++ GLaSS_unityProject/Assets/Scripts/Enemy.cs
@@ -16,12 +16,11 @@ public class Enemy : MonoBehaviour {
 	private float randomY;
 	private Vector2 randomXY;
 
-	// Area Boundaries (TODO later: change to private)
+	// Area Boundaries (TODO later: change to private, keep them public for debugging)
 	private Bounds enemyMovementArea;
 	public Vector2 min;
 	public Vector2 max;
 	public Vector2 size;
-
 
 	// Enemy Properties
 	private Rigidbody2D enemyRigid;
@@ -29,21 +28,18 @@ public class Enemy : MonoBehaviour {
 	private Vector2 posCorrection;
 	private Transform parentTransform;
 
+	// Random motion variables //
 	// Note: need to increase speed + maxVelocity values for larger areas!
 	public enemyMoveType typeOfMovement;
 	public float speed = 600f;
 	public float maxVelocity = 2.8f;
 	public float space = 0.2f;
 
-	//public float frequency = 20f;
-	//public float wavelength = 10f;
-	//private bool enemyRandomMove = true;
-
+	// Circular/Horizontal/Vertical motion variables //
 	public float angle = 0f;
-	public float circleSpeed = (2 * Mathf.PI) / 2;
+	public float circleSpeed = 1.5f;
 	public float radius = 0.05f;
-	public Vector2 center;
-
+	private Vector2 center;
 
 	void Awake() {
 		enemyPosition = GameObject.Find ("Enemy").transform.position;
@@ -63,11 +59,12 @@ public class Enemy : MonoBehaviour {
 		max = enemyMovementArea.max;
 
 		// Extra awake Settings for the circular/horizontal/vertical motion:
-		center = enemyMovementArea.center;
-		if (scaleX == scaleY) { 						// if we are in a square, auto-set the radius
-			radius = (scaleX / 2) - (scaleX / 8); 		// Make it half of the area of movement, and a little bit less to look cuter :)
+		if (typeOfMovement != enemyMoveType.RandomMovement) {
+			center = enemyMovementArea.center;
+			if (scaleX == scaleY) { 						// if we are in a square, auto-set the radius
+				radius = (scaleX / 2) - (scaleX / 8); 		// Make it half of the area of movement, and a little bit less to look cuter :)
+			}
 		}
-
 	}
 		
 	void FixedUpdate() {
@@ -86,8 +83,6 @@ public class Enemy : MonoBehaviour {
 			EnemyHorizontalMove ();
 			break;
 		}
-
-
 	}
 
 	void CreateRandomVector() {
@@ -98,7 +93,7 @@ public class Enemy : MonoBehaviour {
 
 	void EnemyRandomMove() {
 		Vector2 direction = new Vector2(0f, 0f);
-
+		// if the enemy is still within the boundaries, move the player
 		if ((enemyPosition.x > min.x) && (enemyPosition.x < max.x)
 			&& (enemyPosition.y > min.y) && (enemyPosition.y < max.y)) {			 
 			direction = (randomXY - enemyPosition).normalized;	
@@ -112,25 +107,25 @@ public class Enemy : MonoBehaviour {
 				enemyRigid.velocity = new Vector2(enemyRigid.velocity.x, Mathf.Sign(enemyRigid.velocity.y) * maxVelocity);
 			}
 				
-			enemyPosition = transform.position;
+			enemyPosition = transform.position; 	// Update the enemy position
 		} else {
 			if (enemyPosition.x <= min.x) {		// if outside of left barrier
-				posCorrection = new Vector2 (transform.position.x + space, transform.position.y);
+				posCorrection = new Vector2 (transform.position.x + space, transform.position.y); // move it a bit to the right & change direction
 				direction = new Vector2(direction.x+1f, direction.y); // vector -->
 			}
 
 			if (enemyPosition.x >= max.x) {		// if outside of right barrier
-				posCorrection = new Vector2 (transform.position.x - space, transform.position.y);
+				posCorrection = new Vector2 (transform.position.x - space, transform.position.y); // move it a bit to the left & change direction
 				direction = new Vector2(direction.x-1f, direction.y); // vector <--
 			}
 
 			if (enemyPosition.y <= min.y) {		// if outside of bottom barrier
-				posCorrection = new Vector2 (transform.position.x, transform.position.y + space);
+				posCorrection = new Vector2 (transform.position.x, transform.position.y + space); // move it a bit upwards & change direction
 				direction = new Vector2(direction.x, direction.y+1f); // vector ^
 			}
 
 			if (enemyPosition.y >= max.y) {		// if outside of top barrier
-				posCorrection = new Vector2 (transform.position.x, transform.position.y - space);
+				posCorrection = new Vector2 (transform.position.x, transform.position.y - space); // move it a bit downwards & change direction
 				direction = new Vector2(direction.x, direction.y-1f); // vector v
 			}
 
@@ -147,24 +142,19 @@ public class Enemy : MonoBehaviour {
 		angle += circleSpeed * Time.fixedDeltaTime;
 		float x = Mathf.Cos (angle) * radius + center.x;
 		float y = Mathf.Sin (angle) * radius + center.y;
-
-		this.gameObject.transform.position = new Vector2 (x, y);
+		transform.position = new Vector2 (x, y);
 	}
 
 
 	void EnemyVerticalMove() {
-		transform.position = center;
 		angle += circleSpeed * Time.fixedDeltaTime;
-		//float x = Mathf.Cos (angle) * radius + center.x;
-		float y = Mathf.Sin (angle) * radius + center.y;
 		float x = center.x;
+		float y = Mathf.Sin (angle) * radius + center.y;
 		transform.position = new Vector2 (x, y);
 	}
 
 	void EnemyHorizontalMove() {
-		//transform.position = center;
 		angle += circleSpeed * Time.fixedDeltaTime;
-		//float x = Mathf.Cos (angle) * radius + center.x;
 		float x = Mathf.Sin (angle) * radius + center.x;
 		float y = center.y;
 		transform.position = new Vector2 (x, y);
