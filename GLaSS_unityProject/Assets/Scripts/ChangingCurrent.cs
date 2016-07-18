d8bdb6217a567975a31e573af836dd94bd5955d7
prtesla <t.totota@gmail.com>
2016-07-18 16:46:02 +0200

    Force Current that changes with time

---
+++ GLaSS_unityProject/Assets/Scripts/ChangingCurrent.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class ChangingCurrent : MonoBehaviour {
+
+    public float TimeStopped = 1;
+    public float TimeFlowing = 1;
+
+    private AreaEffector2D ae2d;
+    private float Power;
+
+    private List<SpriteRenderer> ObjectsToFade;
+
+    private Color transparent = new Color(1, 1, 1, 0);
+
+	void Start ()
+    {
+        ObjectsToFade = new List<SpriteRenderer>();
+        foreach (SpriteRenderer spr in transform.parent.GetComponentsInChildren<SpriteRenderer>())
+        {
+            if (spr.transform != transform)
+                ObjectsToFade.Add(spr);
+        }
+
+        ae2d = GetComponent<AreaEffector2D>();
+        Power = ae2d.forceMagnitude;
+        Flow();
+	}
+
+    void Flow()
+    {
+        Invoke("Stop", TimeFlowing);
+
+        DOTween.To(() => ae2d.forceMagnitude, x => ae2d.forceMagnitude = x, Power, 0.25f);
+        ChangeColor(Color.white);
+    }
+
+    void Stop()
+    {
+        Invoke("Flow", TimeStopped);
+
+        DOTween.To(() => ae2d.forceMagnitude, x => ae2d.forceMagnitude = x, 0, 0.25f);
+        ChangeColor(transparent);
+    }
+	
+	void Update ()
+    {
+	
+	}
+
+    void ChangeColor(Color color)
+    {
+        foreach (SpriteRenderer spr in ObjectsToFade)
+        {
+            spr.DOColor(color, 0.25f);
+        }
+    }
+}
