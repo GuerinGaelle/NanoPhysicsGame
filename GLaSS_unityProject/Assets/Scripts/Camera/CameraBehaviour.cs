e6d7eff2472575c60d16a3d2b6f44695b2428a83
prtesla <t.totota@gmail.com>
2016-07-19 13:50:29 +0200

    [UPDATE] Camera + menu scene

--- GLaSS_unityProject/Assets/Scripts/Camera/CameraBehaviour.cs
+++ GLaSS_unityProject/Assets/Scripts/Camera/CameraBehaviour.cs
@@ -9,11 +9,13 @@ public class CameraBehaviour : MonoBehaviour {
     public float NormalSmooth = 0.2f;
 
     private GameObject player;
+    private Rigidbody2D playerRigid;
 
 	// Use this for initialization
 	void Start ()
     {
         player = GameManager.Instance.Player.gameObject;
+        playerRigid = player.GetComponent<Rigidbody2D>();
     }
 	
 	// Update is called once per frame
@@ -22,7 +24,9 @@ public class CameraBehaviour : MonoBehaviour {
         if (player == null)
             return;
 
-        Vector3 _newPos = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+        Vector2 velocityAddedForCam = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * 3.14f;
+
+        Vector3 _newPos = new Vector3(player.transform.position.x + velocityAddedForCam.x, player.transform.position.y + velocityAddedForCam.y, -10);
         float _dist = Vector2.Distance(_newPos, transform.position);
 
         if (_dist < 0.2f)
