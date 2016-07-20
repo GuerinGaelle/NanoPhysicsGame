1f968c7926b38811b90d05a613be33d950e55a48
Parvati or Vicky <Parvati or Vicky>
2016-07-20 12:22:15 +0200

    UI popup corrections + update

---
+++ GLaSS_unityProject/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+
+	public static UIManager Instance;
+	//public Canvas UI;
+	public GameManager Game;
+
+	public bool gameTutActive = false;
+
+	// Gameplay Tutorial popups UI
+	public GameObject gravityTutGame;
+	public GameObject inertiaTutGame;
+	public GameObject brownianTutGame;
+	public GameObject vdwTutGame;
+
+	// Scientific Tutorial popups UI: ++++ not done yet
+	public GameObject gravityTutScience;
+	public GameObject inertiaTutScience;
+	public GameObject brownianTutScience;
+	public GameObject vdwTutScience;
+
+	public bool paused = false;
+	//-------------------------------------------//
+
+	void Awake() {
+		if (Instance == null)
+			Instance = this;
+
+		//UI = FindObjectOfType<Canvas> ();
+		Game = FindObjectOfType<GameManager> ();
+		gravityTutGame = GameObject.Find ("Tutorial Gravity");
+		inertiaTutGame = GameObject.Find ("Tutorial Inertia");
+		brownianTutGame = GameObject.Find ("Tutorial Brownian");
+		vdwTutGame = GameObject.Find ("Tutorial VDW");
+
+	}
+
+	void Start() {
+		CloseAllPopups ();
+	}
+
+	void Update() {
+		if (gameTutActive) {																		// if a popup is open, pause the game
+			if (!paused)
+				PauseGame ();
+			// TODO later: disable "pressing" powers while paused!
+			if (Input.GetKeyDown (KeyCode.JoystickButton5) || Input.GetKeyDown (KeyCode.P)) { 		// if u press RB then close the popup and resume the game 
+				CloseAllPopups ();
+				ResumeGame ();
+			}
+		} 
+		else {																// if u press RB then pause & bring up the popups
+			if (Input.GetKeyDown (KeyCode.JoystickButton5) || Input.GetKeyDown (KeyCode.P)) { 		
+				//PauseGame ();
+				// TODO : show tutorials
+				if (Game.isGravityUnlocked && !paused) {
+					gravityTutGame.SetActive (true);
+					PauseGame ();
+				} else if (Game.isGravityUnlocked && paused) {
+					gravityTutGame.SetActive (false);
+					ResumeGame ();
+				}
+			}
+		}
+
+		// power progression: gravity, antibrownian, antiVDW, inertia
+	}
+
+	void CloseAllPopups() {
+		// Gameplay tutorial popups
+		gravityTutGame.SetActive (false);
+		inertiaTutGame.SetActive (false);
+		brownianTutGame.SetActive (false);
+		vdwTutGame.SetActive (false);
+	}
+
+	void PauseGame() {
+		Time.timeScale = 0f;
+		paused = true;
+	}
+
+	void ResumeGame() {
+		Time.timeScale = 1f;
+		gameTutActive = false;
+		paused = false;
+	}
+}
--- GLaSS_unityProject/Assets/Scripts/PowerControl.cs
+++ GLaSS_unityProject/Assets/Scripts/PowerControl.cs
@@ -9,15 +9,13 @@ public class PowerControl : MonoBehaviour {
 		inertia,
 		brownian,
 		VDW,
-		manager
+		//manager
 	}
 
 	public powerIntro power;
 	private float waitTimeUI = 6f;		// to delete if not used!
-	public bool gameTutActive = false;
 
-
-	// Gameplay Tutorial popups UI
+	/*// Gameplay Tutorial popups UI
 	private GameObject gravityTutGame;
 	private GameObject inertiaTutGame;
 	private GameObject brownianTutGame;
@@ -27,41 +25,12 @@ public class PowerControl : MonoBehaviour {
 	private GameObject gravityTutScience;
 	private GameObject inertiaTutScience;
 	private GameObject brownianTutScience;
-	private GameObject vdwTutScience;
+	private GameObject vdwTutScience;*/
 
 	//-------------------------------------------//
 
-	void Awake() {
-		gravityTutGame = GameObject.Find ("Tutorial Gravity");
-		inertiaTutGame = GameObject.Find ("Tutorial Inertia");
-		brownianTutGame = GameObject.Find ("Tutorial Brownian");
-		vdwTutGame = GameObject.Find ("Tutorial VDW");
-	}
 
-	void Start() {
-		
-		// if we are at level 1/tutorial:
-	
-		//GameManager.Instance.LockedAllPowers ();
-		gravityTutGame.SetActive (false);
-		inertiaTutGame.SetActive (false);
-		brownianTutGame.SetActive (false);
-		vdwTutGame.SetActive (false);
-	}
-		
-	void Update() {
-		if (gameTutActive) {
-			Time.timeScale = 0f;
-			if (Input.GetKeyDown(KeyCode.Joystick8Button5) || Input.GetKeyDown(KeyCode.P)) {
-				gravityTutGame.SetActive (false);
-				inertiaTutGame.SetActive (false);
-				brownianTutGame.SetActive (false);
-				vdwTutGame.SetActive (false);
-				Time.timeScale = 1f;
-				gameTutActive = false;
-			}
-		}
-	}
+
 
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.CompareTag ("Player")) {
@@ -74,30 +43,29 @@ public class PowerControl : MonoBehaviour {
 		case powerIntro.gravity:
 			if (!GameManager.Instance.isGravityUnlocked) {
 				GameManager.Instance.UnlockPower ("gravity");
-				gravityTutGame.SetActive (true);
-				gameTutActive = true;
-				//StartCoroutine ("WaitAndCloseUI");
+				UIManager.Instance.gravityTutGame.SetActive (true);
+				UIManager.Instance.gameTutActive = true;
 			}
 			break;
 		case powerIntro.brownian:
 			if (!GameManager.Instance.isBrownianUnlocked) {
 				GameManager.Instance.UnlockPower ("brownian");
-				brownianTutGame.SetActive (true);
-				gameTutActive = true;
+				UIManager.Instance.brownianTutGame.SetActive (true);
+				UIManager.Instance.gameTutActive = true;
 			}
 			break;
 		case powerIntro.inertia:
 			if (!GameManager.Instance.isInertiaUnlocked) {
 				GameManager.Instance.UnlockPower ("inertia");
-				inertiaTutGame.SetActive (true);
-				gameTutActive = true;
+				UIManager.Instance.inertiaTutGame.SetActive (true);
+				UIManager.Instance.gameTutActive = true;
 			}
 			break;
 		case powerIntro.VDW:
 			if (!GameManager.Instance.isVDWUnlocked) {
 				GameManager.Instance.UnlockPower ("vdw");
-				vdwTutGame.SetActive (true);
-				gameTutActive = true;
+				UIManager.Instance.vdwTutGame.SetActive (true);
+				UIManager.Instance.gameTutActive = true;
 			}
 			break;
 		default:
@@ -107,7 +75,7 @@ public class PowerControl : MonoBehaviour {
 		
 	IEnumerator WaitAndCloseUI() {
 		yield return new WaitForSeconds (waitTimeUI);
-		gravityTutGame.SetActive (false);
+		UIManager.Instance.gravityTutGame.SetActive (false);
 	}
 		
 
