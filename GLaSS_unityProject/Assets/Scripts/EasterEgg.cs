add18ed676174f1bf8c02a9d7e22d93f2e3ef545
prtesla <t.totota@gmail.com>
2016-07-21 13:57:43 +0200

    [Update]

---
+++ GLaSS_unityProject/Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EasterEgg : MonoBehaviour {
+
+    private GameObject bullet;
+
+	void Start ()
+    {
+        bullet = Resources.Load<GameObject>("Prefabs/other/Bullet");
+    }
+	
+
+	void Update ()
+    {
+	    if(GameManager.Instance.Player.HasInertia && Input.GetAxis("RT") > 0.8f)
+        {
+            GameObject _bullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+            _bullet.GetComponent<Rigidbody2D>().velocity = GameManager.Instance.Player.transform.right * 30;
+        }
+	}
+}
