e2357772fd836530cac4ba616c4234b1731f4009
prtesla <t.totota@gmail.com>
2016-07-21 17:51:09 +0200

    fiou ! git ^^

---
+++ GLaSS_unityProject/Assets/MoviePlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlayer : MonoBehaviour {
+
+	void Start ()
+    {
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!((MovieTexture)GetComponent<Renderer>().material.mainTexture).isPlaying)
+        {
+            ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+        }
+	}
+}
