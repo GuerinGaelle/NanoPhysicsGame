6d35db233363581a001d4f4ef315b4121a66ec68
prtesla <t.totota@gmail.com>
2016-07-25 12:33:40 +0200

    Bug Fix

--- GLaSS_unityProject/Assets/Scripts/MoviePlayer.cs
+++ GLaSS_unityProject/Assets/Scripts/MoviePlayer.cs
@@ -5,15 +5,12 @@ public class MoviePlayer : MonoBehaviour {
 
 	void Start ()
     {
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).loop = true;
         ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
     }
 	
-	// Update is called once per frame
 	void Update ()
     {
-        if (!((MovieTexture)GetComponent<Renderer>().material.mainTexture).isPlaying)
-        {
-            ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
-        }
+
 	}
 }
