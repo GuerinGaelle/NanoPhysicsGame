55f1644bc75fb4215fd44a244e5d581f336e4456
prtesla <t.totota@gmail.com>
2016-07-27 16:04:55 +0200

    Crushing walls

---
+++ GLaSS_unityProject/Assets/Scripts/taxiMusic.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class taxiMusic : MonoBehaviour {
+
+    private Joint2D joint;
+    private AudioSource source;
+
+    void Start()
+    {
+        joint = GetComponent<Joint2D>();
+        source = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (joint.isActiveAndEnabled && !source.isPlaying)
+            source.Play();
+
+        else if (!joint.isActiveAndEnabled && source.isPlaying)
+            source.Stop();
+    }
+}
