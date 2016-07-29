6e2487dc2d5b8988e07592f3bd1fcf452f355794
prtesla <t.totota@gmail.com>
2016-07-29 17:44:34 +0200

    Big Update 2

--- GLaSS_unityProject/Assets/Scripts/Checkpoint.cs
+++ GLaSS_unityProject/Assets/Scripts/Checkpoint.cs
@@ -10,10 +10,20 @@ public class Checkpoint : MonoBehaviour {
 	}
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.gameObject.name == "Player") {
+            // We do not do anything if we are already in this checkpoint
+            if (GameManager.Checkpoint == energyWellPos)
+                return;
+
 			GameManager.Checkpoint = energyWellPos;
 
+            // AUDIO feedback
             AudioClip sound = Resources.Load<AudioClip>("Music/son/Checkpoint 2");
             GameManager.Instance.GetComponent<AudioSource>().PlayOneShot(sound);
+            // VISUAL feedback
+            GameObject feedback = Resources.Load<GameObject>("Prefabs/Signes_feedbacks/SaveFeedback");
+            GameObject _feedBackInstantiated =  Instantiate(feedback, new Vector3(Camera.current.transform.position.x, Camera.current.transform.position.y, 1), Quaternion.identity) as GameObject;
+            _feedBackInstantiated.transform.SetParent(Camera.current.transform);
+
         }
 	}
 }
